6a3d30c1d28793df3415a011776a54d96aed82b8
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2023-12-06 23:45:32 +0200

    Add files via upload

---
+++ isMadhavArray.js
@@ -0,0 +1,20 @@
+const test = [2, 1, 1, 2, 1, -1, 0, 0, 3, -1, 3, -9, 4, 7, -13, 9];
+log(isMadhavArray(test));
+function isMadhavArray(arr){
+    const goal = [];
+    let n = goal.length;
+    for(let i = 1; i < arr.length; i ++) {
+        n += goal.length;
+        if(arr.slice(n, n + i).length === i) goal.push(arr.slice(n, n + i));
+    }
+    result(goal);
+    function result(arr) {
+        for(let elem of arr) {
+            sum = 0;
+            for(let el of elem) {
+                sum += el;
+            }
+        }
+    }
+    return sum === +goal[0];
+}
